a116a24d2bd7f40c56fa34a16f536d7bc7056379
9068020191 <9068020191@hanyang.ac.kr>
2020-12-09 00:42:05 +0900

    한글 깨짐 문제 해결, 몇몇 js파일 업로드 안된 것 업로드, 오류 페이지 개선

---
+++ static/js/managerpage.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+	$("#refund").click(function () {
+		window.location.href="/refund"
+	})
+
+	$("#monthlysales").click(function () {
+		window.location.href ="/monthlysales"
+	})
+
+	$("#newpassword_input").click(function () {
+		window.location.href ="/change_pw"
+	})
+	$("#bill_option").click(function () {
+		window.location.href ="/change_ordernum"
+	})
+	$("#menumanage").click(function () {
+		window.location.href ="/menumanage"
+	})
+
+})
---
+++ static/js/monthlysales.js
@@ -0,0 +1,58 @@
+function drawChart() {
+	const jsonTable = JSON.parse($("#resultTable").val())
+
+	var tempdata = new google.visualization.DataTable();
+	tempdata.addColumn('datetime', 'DATE')
+	tempdata.addColumn('number', 'PRICE')
+
+	tempdata.addRows(jsonTable.length)
+	for (let i = 0; i < jsonTable.length; i++) {
+		const dt = new Date(jsonTable[i].time)
+
+		tempdata.setCell(i, 0, new Date(dt.getFullYear(),dt.getMonth() , dt.getDate() ))
+		tempdata.setCell(i, 1, parseInt(jsonTable[i].price))
+	}
+
+
+	//월별로 데이터를 묶어서 계산
+	var data = google.visualization.data.group(tempdata, [0], [{ 'column': 1, 'aggregation': google.visualization.data.sum, 'type': 'number' }])
+	var options = {
+		title: '지난 6개월간 월별 매출 (단위: 만원)',
+		titleTextStyle: { fontSize: 15 },
+		chartArea: { 'width': '80%', 'height': '80%' },
+		height: 400,
+		width: 450,
+		legend: { position: 'none' },
+		hAxis: { format: 'MM월' },
+		vAxis: { format: '#만원' }
+
+	};
+
+	//tooltip date format 맞춤
+	var date_formatter = new google.visualization.DateFormat({
+		pattern: "MM월"
+	});
+
+	//tooltip 매출액 format 맞춤
+	date_formatter.format(data, 0)
+	var won_formatter = new google.visualization.NumberFormat({
+		pattern: "#원"
+	});
+	won_formatter.format(data, 1)
+	var view = new google.visualization.DataView(data);
+	view.setColumns([0, {
+		type: 'number',
+		label: data.getColumnLabel(1),
+		calc: function (dt, row) {
+			return { v: dt.getValue(row, 1) / 10000, f: dt.getFormattedValue(row, 1) };
+		}
+	}]);
+
+	var chart = new google.visualization.ColumnChart(document.getElementById('chart_div'));
+	chart.draw(view, options);
+}
+
+$(document).ready(function () {
+	google.charts.load('current', { packages: ['corechart'] });
+	google.charts.setOnLoadCallback(drawChart);
+})
